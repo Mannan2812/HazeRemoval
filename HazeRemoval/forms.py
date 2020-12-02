d37b64b29da3c316ccf32db808be8eb6d9436133
Mannan Bansal <mannan_bansal@yahoo.com>
2020-12-02 13:27:12 +0530

    Added css styling and checkbox for img to choose

--- HazeRemoval/forms.py
+++ HazeRemoval/forms.py
@@ -1,3 +1,14 @@
 from django import forms
 class ImageUploadForm(forms.Form):
-    img = forms.ImageField()
+    OPTIONS = (
+        ('DCP','Dark Channel'),
+        ('RawT','Raw Transmission'),
+        ('RefineT','Refine Transmission'),
+        ('PImg', 'Pre Processes Image'),
+        ('FImg','Final Image')
+    )
+    img = forms.ImageField(label = False)
+    imgShow = forms.MultipleChoiceField(label = False, widget = forms.CheckboxSelectMultiple(attrs= {
+        'class': 'text-white'
+    }), choices = OPTIONS)
+    
--- HazeRemoval/views.py
+++ HazeRemoval/views.py
@@ -11,13 +11,20 @@ def homepage(request):
     if request.method == 'POST':
         form = ImageUploadForm(request.POST,request.FILES)
         if form.is_valid():
+            imgToDisplay = [0,0,0,0,0]
             img = (skimage.io.imread(request.FILES['img']).astype(np.float32))
+            userImgChoice = form.cleaned_data['imgShow']
+            if(userImgChoice.count('DCP')!=0): imgToDisplay[0] = 1
+            if(userImgChoice.count('RawT')!=0): imgToDisplay[1] = 1
+            if(userImgChoice.count('RefineT')!=0): imgToDisplay[2] = 1
+            if(userImgChoice.count('PImg')!=0): imgToDisplay[3] = 1
+            if(userImgChoice.count('FImg')!=0): imgToDisplay[4] = 1 
             print(img.shape)
             n = Noise(img,0,1)
             img = n.addNoise()
             d = dehaze(img)
             d.getDehazedImage()
-            return render(request,'index.html',{'result':True})
+            return render(request,'index.html',{'result':True, 'imgToDisplay' : imgToDisplay})
         return render(request,'index.html')
     else:
         form  = ImageUploadForm()
